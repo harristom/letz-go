c93894f1c9fb196080383f39ab7098bf588a3a7b
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-05-26 19:53:48 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -46,7 +46,7 @@ const NextBusIntentHandler = {
 
         let busStop;
 
-        if (handlerInput.requestEnvelope.request.intent.slots.busStop.resolutionsPerAuthority[0].values[0].value.name) {
+        if (handlerInput.requestEnvelope.request.intent.slots.busStop.resolutionsPerAuthority[0].values[0].value.name.hasOwnProperty) {
             busStop = handlerInput.requestEnvelope.request.intent.slots.busStop.resolutionsPerAuthority[0].values[0].value.name;
         } else {
             const attributesManager = handlerInput.attributesManager;
